8b0c4d9e4ba1c67198b62155720483c89a076e9e
mhmtalakus <mehmetalakus02@icloud.com>
2024-11-26 19:24:41 +0300

    In the script section, the container class was accessed with the query selector, css changes were made, new features were added.

---
+++ dom-navigation-style-change.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+console.log(container);
+
+container.style.backgroundImage = "url('https://c1.wallpaperflare.com/preview/970/253/226/kizkulesi-istanbul-city-bosphorus-kulesi-kiz.jpg')";
+container.style.backgroundSize = "cover";
+container.style.border = "15px solid #D2691E";
+container.style.padding = "20px";
