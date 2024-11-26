ebc94a7d584fa8694cac21b806d690ec4dfd0147
mhmtalakus <mehmetalakus02@icloud.com>
2024-11-26 19:46:14 +0300

    Divs in the container were accessed with querySelectorAll and their CSS properties were given.

--- dom-navigation-style-change.js
+++ dom-navigation-style-change.js
@@ -5,3 +5,13 @@ container.style.backgroundImage = "url('https://c1.wallpaperflare.com/preview/97
 container.style.backgroundSize = "cover";
 container.style.border = "15px solid #D2691E";
 container.style.padding = "20px";
+
+const divAll = document.querySelectorAll(".box");
+console.log(divAll);
+
+for(let i = 0; i < divAll.length; i++){
+    divAll[i].style.backgroundColor = "silver";
+    divAll[i].style.border = "3px solid red";
+    divAll[i].style.height = "auto";
+    divAll[i].style.opacity = "0.5";
+}
